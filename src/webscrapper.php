19e55fa283e3bf2a06fb51d62639679f306f706f
vijayan g <vijayang@salzertechnologies.com>
2017-08-14 18:26:52 +0530

    nitial commit

---
+++ src/webscrapper.php
@@ -0,0 +1,19 @@
+<?php
+  //require 'vendor/autoload.php';
+  use GuzzleHttp\Client;
+
+  function placeholder($someParam)
+  {
+    $client = new Client([
+      'base_uri' => 'http://archive-grbj-2.s3-website-us-west-1.amazonaws.com/',
+      'timeout'  => 5.0,
+    ]);
+
+    # Request / or root
+    $response = $client->request('GET', '/');
+    $body = $response->getBody();
+
+    echo $someParam;
+  }
+
+?>
